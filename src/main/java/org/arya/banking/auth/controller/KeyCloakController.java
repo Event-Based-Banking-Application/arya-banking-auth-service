ca383f5309850310e142b80f8fd57f6999a7430c
kartik1502 <kartikkulkarni1411@gmail.com>
2025-09-13 21:12:01 +0530

    feat: add service to service to authentication for the auth-service and user-service.
    Refs: #73

--- src/main/java/org/arya/banking/auth/controller/KeyCloakController.java
+++ src/main/java/org/arya/banking/auth/controller/KeyCloakController.java
@@ -1,10 +1,7 @@
 package org.arya.banking.auth.controller;
 
-import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
 import org.arya.banking.auth.service.KeyCloakService;
-import org.arya.banking.common.dto.KeyCloakResponse;
-import org.arya.banking.common.model.KeyCloakUser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,11 +11,6 @@ import org.springframework.web.bind.annotation.*;
 public class KeyCloakController {
 
     private final KeyCloakService keyCloakService;
-
-    @PostMapping("/register/users")
-    public ResponseEntity<KeyCloakResponse> registerUser(@RequestBody KeyCloakUser keyCloakUser) {
-        return ResponseEntity.ok(keyCloakService.createKeyCloakUser(keyCloakUser));
-    }
 
     @GetMapping("/authenticate")
     public ResponseEntity<String> authenticate(@RequestParam String username, @RequestParam String password) {
